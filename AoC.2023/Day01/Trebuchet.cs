28d4da3319bdbdb41f170bf43250266e1b0b78fd
Pawel Gruba <pawel.gruba@volue.com>
2023-12-03 00:28:38 +0100

    Add project files.

---
+++ AoC.2023/Day01/Trebuchet.cs
@@ -0,0 +1,56 @@
+﻿namespace AoC._2023.Day01
+{
+    public class Trebuchet
+    {
+        public int Calibrate(string[] inputs)
+        {
+            int result = 0;
+            foreach (var input in inputs)
+            {
+                result += Calibrate(input);
+            }
+            return result;
+        }
+
+        public int Calibrate(string input)
+        {
+            char? firstDigit = null;
+            char? lastDigit = null;
+
+            int firstFoundIndex = -1;
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    firstDigit = input[i];
+                    firstFoundIndex = i;
+                    break;
+                }
+            }
+
+            if (firstFoundIndex == -1)
+            {
+                return 0;
+            }
+
+            for (int i = input.Length - 1; i >= firstFoundIndex; i--)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    lastDigit = input[i];
+                    break;
+                }
+            }
+
+            return lastDigit == null
+                    ? int.Parse($"{firstDigit}{firstDigit}")
+                    : int.Parse($"{firstDigit}{lastDigit}");
+        }
+    }
+}
---
+++ AoC.2023Tests/Day01/TrebuchetTests.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace AoC._2023.Day01.Tests
+{
+    public class TrebuchetTests
+    {
+        private readonly Trebuchet _trebuchet;
+
+        public TrebuchetTests()
+        {
+            _trebuchet = new Trebuchet();
+        }
+
+        [Theory()]
+        [InlineData(new string[] { "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" }, 142)]
+        public void CalibrateTest(string[] input, int result)
+        {
+            // Act
+            int calibration = _trebuchet.Calibrate(input);
+
+            // Assert
+            Assert.Equal(result, calibration);
+        }
+
+        [Theory()]
+        [InlineData("1abc2", 12)]
+        [InlineData("pqr3stu8vwx", 38)]
+        [InlineData("a1b2c3d4e5f", 15)]
+        [InlineData("treb7uchet", 77)]
+        [InlineData("trebuchet", 0)]
+        public void CalibrateSingleString_ReturnsExpected(string input, int result)
+        {
+            Assert.Equal(result, _trebuchet.Calibrate(input));
+        }
+
+        [Fact]
+        public void Calibrate_EmptyString_ReturnsZero()
+        {
+            Assert.Equal(0, _trebuchet.Calibrate(string.Empty));
+        }
+
+        [Fact]
+        public void Calibrate_InputFile_ReturnsExptected()
+        {
+            // Arrange
+            string[] input = System.IO.File.ReadAllLines("Day01/Day01Input.txt");
+
+            // Act
+            int calibration = _trebuchet.Calibrate(input);
+
+            // Assert
+            Assert.Equal(54601, calibration);
+        }
+    }
+}
